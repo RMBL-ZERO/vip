3db99ed34ba76a7169ff41790182dcb645fe2630
RMBL-ZERO <142381754+RMBL-ZERO@users.noreply.github.com>
2023-09-04 19:37:13 +0800

    Update running.sh

--- menu/running.sh
+++ menu/running.sh
@@ -72,38 +72,11 @@ res14() {
 }
 netfilter-persistent
 clear
-echo -e "\033[0;33m ┌──────────────────────────────────────────┐\033[0m"
-echo -e "\033[0;33m │\033[0m          \033[0;32mRESTART SERVICE SERVER\033[0m          \033[0;33m|\033[0m"
-echo -e "\033[0;33m └──────────────────────────────────────────┘\033[0m"
+echo -e "$COLOR1 ┌──────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1 ${NC} ${COLBG1}          ${WH}RESTART SERVICE VPS             ${NC} $COLOR1 $NC"
+echo -e "$COLOR1 └──────────────────────────────────────────┘${NC}"
 echo -e ""
-echo -e "  \033[1;91m service daemon-reload\033[1;37m"
-fun_bar 'res1'
-echo -e "  \033[1;91m service restart nginx\033[1;37m"
-fun_bar 'res2'
-echo -e "  \033[1;91m service restart xray\033[1;37m"
-fun_bar 'res3'
-echo -e "  \033[1;91m service restart rc-local\033[1;37m"
-fun_bar 'res4'
-echo -e "  \033[1;91m service restart client dns\033[1;37m"
-fun_bar 'res5'
-echo -e "  \033[1;91m service restart server dns\033[1;37m"
-fun_bar 'res6'
-echo -e "  \033[1;91m service restart dropbear\033[1;37m"
-fun_bar 'res7'
-echo -e "  \033[1;91m service restart websocket\033[1;37m"
-fun_bar 'res8'
-echo -e "  \033[1;91m service restart openvpn\033[1;37m"
-fun_bar 'res9'
-echo -e "  \033[1;91m service restart cron\033[1;37m"
-fun_bar 'res10'
-echo -e "  \033[1;91m service restart haproxy\033[1;37m"
-fun_bar 'res11'
-echo -e "  \033[1;91m service restart netfilter-persistent\033[1;37m"
-fun_bar 'res12'
-echo -e "  \033[1;91m service restart squid\033[1;37m"
-fun_bar 'res13'
-echo -e "  \033[1;91m service restart badvpn\033[1;37m"
-fun_bar 'res14'
+echo -e "  \033[1;91m Restart All Service... \033[1;37m"
 echo -e ""
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
