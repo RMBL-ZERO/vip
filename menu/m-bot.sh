05713fdeaa42db8a84b42829c1eba26c0e2969bf
RMBL-ZERO <142381754+RMBL-ZERO@users.noreply.github.com>
2023-09-12 23:20:59 +0800

    Add files via upload

--- menu/m-bot.sh
+++ menu/m-bot.sh
@@ -2,7 +2,7 @@
 ipsaya=$(curl -sS ipinfo.io/ip)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/RMBL-ZERO/permission/main/ipmini"
+data_ip="https://raw.githubusercontent.com/casper9/permission/main/ipmini"
 checking_sc() {
     useexp=$(curl -sS $data_ip | grep $ipsaya | awk '{print $3}')
     if [[ $date_list < $useexp ]]; then
@@ -16,7 +16,7 @@ checking_sc() {
         echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
         echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
         echo -e "             \033[0;33mContact Your Admin ${NC}"
-        echo -e "     \033[0;36mTelegram${NC}: https://t.me/rmblvpn1"
+        echo -e "     \033[0;36mTelegram${NC}: https://t.me/CasperGaming"
         echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
         exit
     fi
@@ -35,7 +35,8 @@ apt install python3 python3-pip git speedtest-cli -y
 sudo apt-get install -y p7zip-full
 cd /usr/bin
 clear
-git clone https://github.com/RMBL-ZERO/bot_panel.git
+wget --no-check-certificate http://sc.vpnvip.app/bot.zip &> /dev/null
+7z e -pcasper00 bot.zip &> /dev/null
 unzip kyt.zip &> /dev/null
 pip3 install -r kyt/requirements.txt
 clear
@@ -213,7 +214,8 @@ rm -rf /usr/bin/kyt.zip
 rm -rf /usr/bin/kyt
 sleep 2
 cd /usr/bin
-git clone https://github.com/RMBL-ZERO/bot_panel.git
+wget --no-check-certificate http://sc.vpnvip.app/bot.zip &> /dev/null
+7z e -pcasper00 bot.zip &> /dev/null
 unzip kyt.zip &> /dev/null
 pip3 install -r kyt/requirements.txt
 clear
@@ -319,4 +321,4 @@ rm -rf /usr/bin/kyt/database.db
 systemctl restart kyt 
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
-fi
+fi 
