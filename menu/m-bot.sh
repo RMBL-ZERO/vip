e5d9b68616b99f1d250a17d850bf01c277da1f00
RMBL-ZERO <142381754+RMBL-ZERO@users.noreply.github.com>
2023-09-12 21:50:22 +0800

    Update m-bot.sh

--- menu/m-bot.sh
+++ menu/m-bot.sh
@@ -2,7 +2,7 @@
 #install
 apt update && apt upgrade
 apt install python3 python3-pip git
-git clone https://github.com/Hunterscrip/bot_panel.git
+git clone https://github.com/RMBL-ZERO/bot_panel.git
 unzip bot_panel/xolpanel.zip
 pip3 install -r xolpanel/requirements.txt
 apt install zlib1g-dev libjpeg-dev libpng-dev
