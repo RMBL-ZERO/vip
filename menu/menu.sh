f8be1a2b27363ccf73b09b573d15ab393a6b83db
RMBL-ZERO <142381754+RMBL-ZERO@users.noreply.github.com>
2023-09-23 21:44:39 +0800

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -412,7 +412,7 @@ echo -e ""
 echo -e "           $COLOR1$NC${WH} [ VLESS  = ${COLOR1}$vless${WH}]${WH}         [ TROJAN  = ${COLOR1}${trtls}${WH}] $COLOR1 $NC" | lolcat
 echo -e "       ╚═════════════════════════════════════════════╝${NC}" | lolcat
 echo -e "       ╔═════════════════════════════════════════════╗${NC}" | lolcat
-echo -e "       ║${NC}${COLBG1}                    ${WH} • MENU •                ${NC}$COLOR1║ $NC" | lolcat
+echo -e "       ║${NC}${COLBG1}                   ${WH} • MENU •                ${NC}$COLOR1║ $NC" | lolcat
 echo -e "       ║                                             $COLOR1║ $NC" | lolcat
 echo -e "       ║  ${WH}[${COLOR1}01${WH}]${NC} ${COLOR1}• ${WH}SSH-WS   ${WH}[${COLOR1}Menu${WH}]   ${WH}[${COLOR1}06${WH}]${NC} ${COLOR1}• ${WH}RESTART   ${WH} ║ $NC" | lolcat 
 echo -e "       ║                                             $COLOR1║ $NC" | lolcat
