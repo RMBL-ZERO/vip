411678c0dedc131cfaf61386db183e163ac8a67b
RMBL-ZERO <142381754+RMBL-ZERO@users.noreply.github.com>
2023-10-08 21:22:38 +0800

    Update menux.sh

--- menu/menux.sh
+++ menu/menux.sh
@@ -163,8 +163,7 @@ Info="${Green_font_prefix}(Registered)${Font_color_suffix}"
 Error="${Green_font_prefix}${Font_color_suffix}${Red_font_prefix}[EXPIRED]${Font_color_suffix}"
 
 today=$(date -d "0 days" +"%Y-%m-%d")
-Exp1=$(curl https://raw.githubusercontent.com/RMBL-ZERO/permission/main/ipmini | grep $MYIP | awk '{print $4}')
-if [[ $today < $Exp1 ]]; then
+if [[ $today < $Exp2 ]]; then
     sts="${Info}"
 else
     sts="${Error}"
