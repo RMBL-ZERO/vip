06be06b60c6baefd0fff1ca238194ef46bf0bb24
RMBL-ZERO <142381754+RMBL-ZERO@users.noreply.github.com>
2023-10-23 01:19:24 +0800

    Update m-bot.sh

--- menu/m-bot.sh
+++ menu/m-bot.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
+NS=$( cat /etc/xray/dns )
+PUB=$( cat /etc/slowdns/server.pub )
+domain=$(cat /etc/xray/domain)
+#color
+grenbo="\e[92;1m"
+NC='\e[0m'
 #install
 apt update && apt upgrade
 apt install python3 python3-pip git
-git clone https://github.com/Keposekali/bot_panel.git
-unzip bot_panel/kyt.zip
+cd /usr/bin
+wget https://raw.githubusercontent.com/Keposekali/bot_panel/main/bot.zip
+unzip bot.zip
+mv bot/* /usr/bin
+chmod +x /usr/bin/*
+rm -rf bot.zip
+clear
+wget https://raw.githubusercontent.com/Keposekali/bot_panel/main/kyt.zip
+unzip kyt.zip
 pip3 install -r kyt/requirements.txt
-apt install zlib1g-dev libjpeg-dev libpng-dev
-pip3 install pillow
 
 #isi data
 echo ""
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " \e[1;97;101m          ADD BOT PANEL          \e[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "${grenbo}Tutorial Creat Bot and ID Telegram${NC}"
+echo -e "${grenbo}[*] Creat Bot and Token Bot : @BotFather${NC}"
+echo -e "${grenbo}[*] Info Id Telegram : @MissRose_bot , perintah /info${NC}"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 read -e -p "[*] Input your Bot Token : " bottoken
 read -e -p "[*] Input Your Id Telegram :" admin
-read -e -p "[*] Input Your Domain :" domain
-echo -e BOT_TOKEN='"'$bottoken'"' >> /root/kyt/var.txt
-echo -e ADMIN='"'$admin'"' >> /root/kyt/var.txt
-echo -e DOMAIN='"'$domain'"' >> /root/kyt/var.txt
+echo -e BOT_TOKEN='"'$bottoken'"' >> /usr/bin/kyt/var.txt
+echo -e ADMIN='"'$admin'"' >> /usr/bin/kyt/var.txt
+echo -e DOMAIN='"'$domain'"' >> /usr/bin/kyt/var.txt
+echo -e PUB='"'$PUB'"' >> /usr/bin/kyt/var.txt
+echo -e HOST='"'$NS'"' >> /usr/bin/kyt/var.txt
 clear
-echo "Done"
-echo "Your Data Bot"
-echo -e "==============================="
-echo "DOMAIN         : $bottoken"
-echo "Email          : $admin"
-echo "Api Key        : $domain"
-echo -e "==============================="
-echo "Setting done"
 
 cat > /etc/systemd/system/kyt.service << END
 [Unit]
@@ -32,7 +43,7 @@ Description=Simple kyt - @kyt
 After=network.target
 
 [Service]
-WorkingDirectory=/root
+WorkingDirectory=/usr/bin
 ExecStart=/usr/bin/python3 -m kyt
 Restart=always
 
@@ -42,5 +53,19 @@ END
 
 systemctl start kyt 
 systemctl enable kyt
+systemctl restart kyt
+cd /root
+rm -rf kyt.sh
+echo "Done"
+echo "Your Data Bot"
+echo -e "==============================="
+echo "Token Bot         : $bottoken"
+echo "Admin          : $admin"
+echo "Domain        : $domain"
+echo "Pub            : $PUB"
+echo "Host           : $NS"
+echo -e "==============================="
+echo "Setting done"
 clear
+
 echo " Installations complete, type /menu on your bot"
