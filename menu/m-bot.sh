8247b94321cfdf12a261ec83eb65244e28f68d61
RMBL-ZERO <142381754+RMBL-ZERO@users.noreply.github.com>
2023-10-24 08:59:18 +0800

    Update m-bot.sh

--- menu/m-bot.sh
+++ menu/m-bot.sh
@@ -1,71 +1,84 @@
 #!/bin/bash
-NS=$( cat /etc/xray/dns )
-PUB=$( cat /etc/slowdns/server.pub )
-domain=$(cat /etc/xray/domain)
-#color
-grenbo="\e[92;1m"
+DF='\e[39m'
+RED='\033[0;31m'
+Bold='\e[1m'
+Blink='\e[5m'
+yell='\e[33m'
+red='\e[31m'
+green='\e[32m'
+blue='\e[34m'
+PURPLE='\e[35m'
+cyan='\e[36m'
+Lred='\e[91m'
+Lyellow='\e[93m'
+Lgreen='\e[92m'
 NC='\e[0m'
-#install
-apt update && apt upgrade
-apt install python3 python3-pip git
-cd /usr/bin
-wget https://raw.githubusercontent.com/Keposekali/bot_panel/main/bot.zip
-unzip bot.zip
-mv bot/* /usr/bin
-chmod +x /usr/bin/*
-rm -rf bot.zip
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+LIGHT='\033[0;37m'
+grenbo="\e[92;1m"
+red() { echo -e "\\033[32;1m${*}\\033[0m"; }
+# Getting
+#satusbot
+bot_kyt=$(systemctl status kyt | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+# STATUS SERVICE  BOT 
+if [[ $bot_kyt == "running" ]]; then 
+   status_bot="${GREEN}Online$NC${c} $NC"
+else
+   status_bot="${RED}Offline${NC} "
+fi
+#####
+KIRI="\033[1;32m>\033[1;33m>\033[1;31m>\033[1;31m$NC"
+ipsaya=$(wget -qO- ipinfo.io/ip)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/RMBL-ZERO/permission/main/ipmini"
+checking_sc() {
+  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
+  if [[ $date_list < $useexp ]]; then
+    echo -ne
+  else
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e ""
+    echo -e "            ${RED}PERMISSION DENIED !${NC}"
+    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
+    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
+    echo -e "             \033[0;33mContact Admin :${NC}"
+    echo -e "      \033[0;36mTelegram${NC} t.me/rmblvpn1"
+    #echo -e "      ${GREEN}WhatsApp${NC} wa.me/"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    exit
+  fi
+}
+checking_sc
+
+echo -e "\e[32mloading...\e[0m"
 clear
-wget https://raw.githubusercontent.com/Keposekali/bot_panel/main/kyt.zip
-unzip kyt.zip
-pip3 install -r kyt/requirements.txt
-
-#isi data
-echo ""
-echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          ADD BOT PANEL          \e[0m"
-echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "${grenbo}Tutorial Creat Bot and ID Telegram${NC}"
-echo -e "${grenbo}[*] Creat Bot and Token Bot : @BotFather${NC}"
-echo -e "${grenbo}[*] Info Id Telegram : @MissRose_bot , perintah /info${NC}"
-echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-read -e -p "[*] Input your Bot Token : " bottoken
-read -e -p "[*] Input Your Id Telegram :" admin
-echo -e BOT_TOKEN='"'$bottoken'"' >> /usr/bin/kyt/var.txt
-echo -e ADMIN='"'$admin'"' >> /usr/bin/kyt/var.txt
-echo -e DOMAIN='"'$domain'"' >> /usr/bin/kyt/var.txt
-echo -e PUB='"'$PUB'"' >> /usr/bin/kyt/var.txt
-echo -e HOST='"'$NS'"' >> /usr/bin/kyt/var.txt
-clear
-
-cat > /etc/systemd/system/kyt.service << END
-[Unit]
-Description=Simple kyt - @kyt
-After=network.target
-
-[Service]
-WorkingDirectory=/usr/bin
-ExecStart=/usr/bin/python3 -m kyt
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-END
-
-systemctl start kyt 
-systemctl enable kyt
-systemctl restart kyt
-cd /root
-rm -rf kyt.sh
-echo "Done"
-echo "Your Data Bot"
-echo -e "==============================="
-echo "Token Bot         : $bottoken"
-echo "Admin          : $admin"
-echo "Domain        : $domain"
-echo "Pub            : $PUB"
-echo "Host           : $NS"
-echo -e "==============================="
-echo "Setting done"
-clear
-
-echo " Installations complete, type /menu on your bot"
+MYIP=$(wget -qO- ipinfo.io/ip);
+clear 
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "         • MENU BOT PANEL •         "
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "" 
+echo -e " ${KIRI} Status Bot ( ${status_bot} )"
+echo -e ""
+echo -e " [\e[36m•1\e[0m] Add Bot Akun"
+echo -e " [\e[36m•2\e[0m] Delete Bot Panel"
+echo -e " [\e[36m•3\e[0m] Stop Bot Panel"
+echo -e " [\e[36m•4\e[0m] Restart Bot Panel"
+echo -e "" 
+echo -e " [\e[31m•0\e[0m] \e[31mBack To Menu\033[0m"
+echo -e "" 
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+read -p " Select menu :  "  opt
+echo -e   ""
+case $opt in
+01 | 1) clear ; wget https://raw.githubusercontent.com/RMBL-ZERO/rmbl/main/rmbl.sh && chmod +x rmbl.sh && ./rmbl.sh ;;
+02 | 2) clear ; hapus-bot ;;
+02 | 3) clear ; stop-bot ;;
+00 | 4) clear ; restart-bot ;;
+*) clear ; menu ;;
+esac
