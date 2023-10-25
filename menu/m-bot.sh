6053476286ef33ef81d4449fa6eab2671a1cf1c8
RMBL-ZERO <142381754+RMBL-ZERO@users.noreply.github.com>
2023-10-25 22:47:11 +0800

    Update m-bot.sh

--- menu/m-bot.sh
+++ menu/m-bot.sh
@@ -2,9 +2,9 @@
 #install
 apt update && apt upgrade
 apt install python3 python3-pip git
-git clone https://github.com/Keposekali/bot_panel.git
-unzip bot_panel/kyt.zip
-pip3 install -r kyt/requirements.txt
+git clone https://github.com/Hunterscrip/bot_panel.git
+unzip bot_panel/xolpanel.zip
+pip3 install -r xolpanel/requirements.txt
 apt install zlib1g-dev libjpeg-dev libpng-dev
 pip3 install pillow
 
@@ -13,9 +13,9 @@ echo ""
 read -e -p "[*] Input your Bot Token : " bottoken
 read -e -p "[*] Input Your Id Telegram :" admin
 read -e -p "[*] Input Your Domain :" domain
-echo -e BOT_TOKEN='"'$bottoken'"' >> /root/kyt/var.txt
-echo -e ADMIN='"'$admin'"' >> /root/kyt/var.txt
-echo -e DOMAIN='"'$domain'"' >> /root/kyt/var.txt
+echo -e BOT_TOKEN='"'$bottoken'"' >> /root/xolpanel/var.txt
+echo -e ADMIN='"'$admin'"' >> /root/xolpanel/var.txt
+echo -e DOMAIN='"'$domain'"' >> /root/xolpanel/var.txt
 clear
 echo "Done"
 echo "Your Data Bot"
@@ -26,21 +26,21 @@ echo "Api Key        : $domain"
 echo -e "==============================="
 echo "Setting done"
 
-cat > /etc/systemd/system/kyt.service << END
+cat > /etc/systemd/system/xolpanel.service << END
 [Unit]
-Description=Simple kyt - @kyt
+Description=Simple XolPanel - @XolPanel
 After=network.target
 
 [Service]
 WorkingDirectory=/root
-ExecStart=/usr/bin/python3 -m kyt
+ExecStart=/usr/bin/python3 -m xolpanel
 Restart=always
 
 [Install]
 WantedBy=multi-user.target
 END
 
-systemctl start kyt 
-systemctl enable kyt
+systemctl start xolpanel 
+systemctl enable xolpanel
 clear
 echo " Installations complete, type /menu on your bot"
